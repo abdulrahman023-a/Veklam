b6fa63c905951e1720ac2bb405964b1898cd967e
abdulrahman023-a <abdulrahmanawl20@gmail.com>
2026-05-09 20:43:53 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration (Placeholders for user to fill)
+// In a real scenario, these would be provided by the user or set up via Firebase Console
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// We will use the Firebase CDN in the main HTML file
+// This file will serve as a template for the user to input their own credentials
+console.log("Firebase config loaded. Please update with your actual credentials in js/firebase-config.js");
