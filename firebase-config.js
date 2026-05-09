450ad9ce6fd1e24c07846b91f4af1818864feb2a
abdulrahman023-a <abdulrahmanawl20@gmail.com>
2026-05-09 20:52:39 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,8 @@
-// Firebase Configuration (Placeholders for user to fill)
-// In a real scenario, these would be provided by the user or set up via Firebase Console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA5jYa-EtqYQ_OK6bUZoLIN5K-Ik_BlKB4",
+  authDomain: "veklam-2d88b.firebaseapp.com",
+  projectId: "veklam-2d88b",
+  storageBucket: "veklam-2d88b.firebasestorage.app",
+  messagingSenderId: "67462658094",
+  appId: "1:67462658094:web:e28951272e2deb17f007bf"
 };
-
-// We will use the Firebase CDN in the main HTML file
-// This file will serve as a template for the user to input their own credentials
-console.log("Firebase config loaded. Please update with your actual credentials in js/firebase-config.js");
