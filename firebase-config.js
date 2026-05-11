f624cc56de9634fbb0260f3d230fa50e5c2e1c2c
abdulrahman023-a <abdulrahmanawl20@gmail.com>
2026-05-12 02:34:56 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA5jYa-EtqYQ_OK6bUZoLIN5K-Ik_BlKB4",
-  authDomain: "veklam-2d88b.firebaseapp.com",
-  projectId: "veklam-2d88b",
-  storageBucket: "veklam-2d88b.firebasestorage.app",
-  messagingSenderId: "67462658094",
-  appId: "1:67462658094:web:e28951272e2deb17f007bf"
-};
